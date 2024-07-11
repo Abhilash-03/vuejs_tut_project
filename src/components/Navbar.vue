e9caba8b25016a7285917e32a7424f1e3f13c46d
Abhilash <abhilashjha335@gmail.com>
2024-07-11 21:32:42 +0530

    update navbar and add not-found page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,12 @@
 <script setup>
  import logo from '@/assets/img/image.png'
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+}
+
 </script>
 
 <template>
@@ -21,19 +27,16 @@ import { RouterLink } from 'vue-router';
               <div class="flex space-x-2">
                 <RouterLink
                   to="/"
-                  class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</RouterLink
-                >
-                <RouterLink
+                  :class="[isActiveLink('/') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white' ,'text-white',  'rounded-md', 'px-3', 'py-2']"
+                  >Home</RouterLink>
+                  <RouterLink
                   to="/jobs"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</RouterLink
-                >
-                <RouterLink
+                  :class="[isActiveLink('/jobs') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white' ,'text-white',  'rounded-md', 'px-3', 'py-2']"
+                  >Jobs</RouterLink>
+                  <RouterLink
                   to="/jobs/add"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</RouterLink
-                >
+                  :class="[isActiveLink('/jobs/add') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white' ,'text-white',  'rounded-md', 'px-3', 'py-2']"
+                  >Add Job</RouterLink>
               </div>
             </div>
           </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from '@/views/HomeView.vue';
 import JobView from '@/views/JobsView.vue';
+import NotFound from '@/views/NotFoundPage.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: '/jobs',
             name: 'job',
             component: JobView
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component: NotFound
         }
     ]
 })
