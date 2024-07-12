6aeb8848199472be347f0ec9947eaadea2c0257b
Abhilash <abhilashjha335@gmail.com>
2024-07-12 23:17:35 +0530

    update jobListing component use reactive state and add json-server and spinner

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,9 +1,15 @@
 <script setup>
-import jobData from '@/job.json';
-import { ref, defineProps, computed } from 'vue';
+import jobData from '@/jobs.json';
+import { ref, defineProps, onMounted, reactive } from 'vue';
 import JobListCard from './JobListCard.vue';
 import { RouterLink } from 'vue-router';
-const jobs = ref(jobData);
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import axios from 'axios';
+
+const state = reactive({
+    jobs: [],
+    isLoading: true
+})
 
 defineProps({
     limit: Number,
@@ -13,6 +19,17 @@ defineProps({
     }
 })
 
+onMounted(async() => {
+    try {
+        const response = await axios.get('http://localhost:4000/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.log("Error occured while fetching data ", error);
+    } finally {
+    state.isLoading = false;
+  }
+})
+
 
 </script>
 
@@ -20,8 +37,12 @@ defineProps({
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
+                <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListCard v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+                <JobListCard v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
         </div>
         </div>
     </section>
